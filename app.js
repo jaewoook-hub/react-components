605a3f89bcb9e791ac577fbe124ae07a3b957335
Richard Ok <jaeok2017@u.northwestern.edu>
2020-08-31 15:29:40 -0500

    Assignment Complete

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-// TODO
+class GroceryListItem extends React.Component { // TodoListItem
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false,
+      hovered: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  onMouseOver() {
+    this.setState({
+      hovered: true
+    });
+  }
+
+  onMouseOut() {
+    this.setState({
+      hovered: false
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none',
+      fontWeight: this.state.hovered ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onMouseOver.bind(this)} onMouseOut={this.onMouseOut.bind(this)} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+var GroceryListItems = (props) => (
+  <ul>
+    {props.groceries.map(item =>
+    <GroceryListItem item={item} />
+  )}
+  </ul>
+);
+
+var GroceryList = () => (
+  <div>
+  <GroceryListItems groceries={['Eggs', 'Ham', 'Cheese']} />
+  </div>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
